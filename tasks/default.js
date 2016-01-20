fcb315c0f9b2c859874601b7796a36c608f18fd6
Dominik Schmidt <visualcake@me.com>
2016-01-20 11:12:50 +0100

    :nail_care: beautify default task /w run-sequence

--- tasks/default.js
+++ tasks/default.js
@@ -1,23 +1,16 @@
 import gulp from 'gulp';
 import { src } from './config';
 import { join } from 'path';
+import sequence from 'run-sequence';
 
 gulp.task('default', ['clean'], () => {
-  gulp.start('server');
-  gulp.start('scripts');
-  gulp.start('fonts');
+  sequence(
+    'clean',
+    ['scripts', 'fonts', 'images', 'markup', 'styles'],
+    'server'
+  );
 
-  const initTask = (files, task) => {
-    if ( typeof task === 'string' ) {
-      gulp.start(task);
-      gulp.watch(files, [ task ]);
-    } else {
-      task.map(subtask => gulp.start(subtask));
-      gulp.watch(files, task);
-    }
-  };
-
-  initTask(join(src, 'images', '**/*'), 'images');
-  initTask(join(src, 'html', '**/*.html'), 'markup');
-  initTask(join(src, 'styles', '**/*.{scss,sass}'), 'styles');
+  gulp.watch(join(src, 'images', '**/*'), ['images']);
+  gulp.watch(join(src, 'html', '**/*.html'), ['markup']);
+  gulp.watch(join(src, 'styles', '**/*.{scss,sass}'), ['styles']);
 });
