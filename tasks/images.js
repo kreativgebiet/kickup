469d7ba80da249b346afa7f93f63d06b74f910d2
Jonas Schmitt <jonas@kreativgebiet.com>
2017-06-20 14:07:49 +0200

    :bug: fix image pipeline

--- tasks/images.js
+++ tasks/images.js
@@ -8,14 +8,18 @@ import browserSync from './connect';
 import { server, watch, imagemin } from '../config';
 
 gulp.task('images', () => {
-  gulp.src(watch.images)
+  const images = gulp.src(watch.images)
     .pipe(changed(join(server.dest, 'images')))
     .pipe(gulp.dest(join(server.dest, 'images')))
     .pipe(browserSync.stream());
+
+  return images;
 });
 
 gulp.task('images:build', () => {
-  gulp.src(watch.images)
+  const buildImages = gulp.src(watch.images)
     .pipe(imageMinify(imagemin))
     .pipe(gulp.dest(join(server.dest, 'images')));
+
+  return buildImages;
 });
