a9b4481f14c117c6687d0423960005b501426857
Jonas Schmitt <jonas@kreativgebiet.com>
2017-12-19 16:57:35 +0100

    add router - only load scripts which are needed

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+/** import external dependencies */
+import 'jquery';
+import './util/lettering';
+
+/** import local dependencies */
+import Router from './util/Router';
+import common from './routes/common';
+import home from './routes/home';
+
+/**
+ * Populate Router instance with DOM routes
+ * @type {Router} routes - An instance of our router
+ */
+const routes = new Router({
+  /** All pages */
+  common,
+  /** Home page */
+  home,
+});
+
+/** Load Events */
+jQuery(document).ready(() => routes.loadEvents());
